b7d0591b83460ab824907f2f5564f3423ccbb9d7
Élafru <vigiraud@insa-rennes.fr>
2017-10-25 09:43:30 +0200

    Entité gestionnaireES

---
+++ cyclesMemoire.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+ENTITY gestionnaireES IS
+	PORT(
+			PortCom : INOUT std_logic_vector(15 DOWNTO 0)
+		);
+END gestionnaireES;
+	
+ARCHITECTURE base OF gestionnaireES IS
+BEGIN
+	
+END base;
--- decodage.vhd
+++ decodage.vhd
@@ -30,27 +30,27 @@ BEGIN
 				adresserom <= adresse(integer(ceil(log2(real(taille_ROM)))) DOWNTO 0);
 			END IF;
 		END IF;
-			
+
 		IF adresse(2+integer(ceil(log2(real(taille_ROM))))) = '0' THEN
 			IF adresse(1+integer(ceil(log2(real(taille_ROM))))) = '1' THEN				-- Bits de poids fort : 01
 				CSrom <= '1'; CSram1 <= '0'; CSram2 <= '1'; CSram3 <= '1';				-- RAM n°= 1
 				adresseram1 <= adresse(integer(ceil(log2(real(taille_RAM)))) DOWNTO 0);
 			END IF;
 		END IF;
-			
+
 		IF adresse(2+integer(ceil(log2(real(taille_ROM))))) = '1' THEN
 			IF adresse(1+integer(ceil(log2(real(taille_ROM))))) = '0' THEN				-- Bits de poids fort : 10
 				CSrom <= '1'; CSram1 <= '1'; CSram2 <= '0'; CSram3 <= '1';				-- RAM n°= 2
 				adresseram2 <= adresse(integer(ceil(log2(real(taille_RAM)))) DOWNTO 0);
 			END IF;
 		END IF;
-			
+
 		IF adresse(2+integer(ceil(log2(real(taille_ROM))))) = '1' THEN
 			IF adresse(1+integer(ceil(log2(real(taille_ROM))))) = '1' THEN				-- Bits de poids fort : 11
 				CSrom <= '1'; CSram1 <= '1'; CSram2 <= '1'; CSram3 <= '0';				-- RAM n°= 3
 				adresseram3 <= adresse(integer(ceil(log2(real(taille_RAM)))) DOWNTO 0);
 			END IF;
 		END IF;
-				
+
 	END PROCESS;
 END base;
